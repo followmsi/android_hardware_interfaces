922c355aaf45db96ad0025bf4ad48fc6e9d815ed
followmsi <followmsi@gmail.com>
2019-04-19 16:25:48 +0200

    Revert "hwc2on1: workaround fix msm8960 no display"
    This reverts commit 0f37513c9aa8d5f9e33af0bba1960e11af6cc72b.

--- graphics/composer/2.1/utils/hwc2on1adapter/include/hwc2on1adapter/HWC2On1Adapter.h
+++ graphics/composer/2.1/utils/hwc2on1adapter/include/hwc2on1adapter/HWC2On1Adapter.h
@@ -264,7 +264,6 @@ private:
 
             bool hasChanges() const;
             HWC2::Error set(hwc_display_contents_1& hwcContents);
-            HWC2::Error set2(hwc_display_contents_1& hwcContents, bool prepare);
             void addRetireFence(int fenceFd);
             void addReleaseFences(const hwc_display_contents_1& hwcContents);
 
